62b96fea8942cc7e4445fe28f84cee7f9e06e917
Preetam <nihal@Nihal.local>
2023-08-18 15:47:56 +0530

    feign makes it easy to call rest api

--- src/main/java/com/preetamlahre/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ src/main/java/com/preetamlahre/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -2,7 +2,8 @@ package com.preetamlahre.microservices.currencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.openfeign.EnableFeignClients;
+@EnableFeignClients
 @SpringBootApplication
 public class CurrencyConversionServiceApplication {
 
