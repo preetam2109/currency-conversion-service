56027b39eb5b86330f82e65165d9c1361fedb745
Preetam <nihal@Nihal.local>
2023-08-18 15:47:56 +0530

    feign makes it easy to call rest api

--- src/main/java/com/preetamlahre/microservices/currencyconversionservice/CurrencyConversionController.java
+++ src/main/java/com/preetamlahre/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -3,6 +3,7 @@ package com.preetamlahre.microservices.currencyconversionservice;
 import java.math.BigDecimal;
 import java.util.HashMap;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,6 +12,8 @@ import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class CurrencyConversionController {
+	@Autowired
+	private CurrencyExchangeProxy proxy;
 	@GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
 	public CurrencyConversion calculateCurrencyConversion(@PathVariable String from
 							,@PathVariable String to,
@@ -32,7 +35,24 @@ public class CurrencyConversionController {
 		return new CurrencyConversion(currencyConversion.getId(),from,to,
 				currencyConversion.getConversionMultiple(),quantity,
 				quantity.multiply(currencyConversion.getConversionMultiple()), 
-				currencyConversion.getEnvironment());
+				currencyConversion.getEnvironment()+" "+"rest template");
+		
+	}
+	@GetMapping("/currency-conversion-feign/from/{from}/to/{to}/quantity/{quantity}")
+	public CurrencyConversion calculateCurrencyConversionFeign(@PathVariable String from
+							,@PathVariable String to,
+							@PathVariable BigDecimal quantity
+							)
+
+	{
+		
+		CurrencyConversion currencyConversion = proxy.retrieveExchangeValue(from, to);
+		
+		
+		return new CurrencyConversion(currencyConversion.getId(),from,to,
+				currencyConversion.getConversionMultiple(),quantity,
+				quantity.multiply(currencyConversion.getConversionMultiple()), 
+				currencyConversion.getEnvironment()+" "+"feign");
 		
 	}
 
